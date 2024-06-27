8ba8a63f55adc0f6ff57dcc580b56c207cf0ccd4
Alvaro Muñoz <pwntester@github.com>
2024-06-27 10:33:54 +0200

    tests

---
+++ java/actions-tests/src/main/java/org/pwntester/App.java
@@ -0,0 +1,10 @@
+package org.pwntester;
+import java.util.Map;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Map<String, String> env = System.getenv();
+        env.forEach((k, v) -> System.err.println(k + ":" + v));
+    }
+}
